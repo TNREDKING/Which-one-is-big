9345d63b1873e2e57c7a5bfdac7fa8bd16bdf759
TNREDKING <126704235+TNREDKING@users.noreply.github.com>
2023-03-07 20:00:12 +0530

    Add files via upload

---
+++ 2no which is big.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout << " enter a:\n";
+    cin >> a;
+    cout << "enter b:\n";
+    cin >> b;
+    if(a>b)
+    {
+    cout<<"a is biggest no \n";
+    }
+    else
+   { 
+    cout<<"b is biggest no \n";
+   }
+    return 0;
+}
